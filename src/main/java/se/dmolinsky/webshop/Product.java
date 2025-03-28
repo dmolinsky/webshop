a8e7da04ba4abb5f8bab10d410463abddac1cdfa
dmolinsky <diana.molinsky@iths.se>
2025-03-28 14:18:32 +0100

    added entity class Product

---
+++ src/main/java/se/dmolinsky/webshop/Product.java
@@ -0,0 +1,79 @@
+package se.dmolinsky.webshop;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product name cannot be blank")
+    @Size(min = 3, max = 255, message = "Product name must be between 3 and 255 characters")
+    private String name;
+
+    @NotNull(message = "Price cannot be null")
+    @Column(precision = 10, scale = 2)
+    private Double price;
+
+    @Size(max = 100, message = "Category should not exceed 100 characters")
+    private String category;
+
+    @Lob
+    @Column(name = "image")
+    private byte[] image;
+
+    public Product() {}
+
+    public Product(String name, Double price, String category, byte[] image) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.image = image;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
